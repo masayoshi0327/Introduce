6f6b59c509fcda1de2edfebb46f159bc479b6a62
masayoshi0327 <75524615+masayoshi0327@users.noreply.github.com>
2021-02-11 17:02:12 +0900

    Initial Commit

---
+++ Introduce/IntroduceApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroduceApp.swift
+//  Introduce
+//
+//  Created by 澤崎正佳 on 2021/02/11.
+//
+
+import SwiftUI
+
+@main
+struct IntroduceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
